46173e0f459c5bcb758577e3091454ed3b4905c2
Ckyever Gaviola <ckyeverg@gmail.com>
2025-11-01 21:00:23 +1100

    Add reset button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import resetIcon from "./assets/history.svg";
 
 import { exampleResume } from "./data.js";
 import Resume from "./components/Resume.jsx";
@@ -12,6 +13,11 @@ function App() {
     <div className="app">
       <Sidebar resumeData={resumeData} setResumeData={setResumeData} />
       <Resume resumeData={resumeData} />
+      {/* Refreshing the page here instead of reloading default data because the current design does not allow us to reload the data into the input fields as well */}
+      <button className="reset-button" onClick={() => window.location.reload()}>
+        <img src={resetIcon} alt="reset icon" />
+        Reset
+      </button>
     </div>
   );
 }
